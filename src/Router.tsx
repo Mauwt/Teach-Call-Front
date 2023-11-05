2264819a5d43e0c2c68598315f167cba904c4536
Mauwt <david.jacobo@utec.edu.pe>
2023-11-05 03:15:30 -0500

    schedule

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,6 +9,7 @@ import TeacherRegister from './pages/TeacherRegister';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import ProfessorTour from './pages/ProfessorTour';
+import TeacherDashboard from './pages/TeacherDashboard';
 
 export default function AppRouter() {
   return (
@@ -22,12 +23,14 @@ export default function AppRouter() {
       <Route path="/teacher-login" element={<TeacherLogin />} />
       <Route path="/teacher-register" element={<TeacherRegister />} />
       <Route path="/dashboard/student" element={<Dashboard />} />
+      <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
+
       {/* Rutas de acceso unico a ESTUDIANTES */}
       <Route element={<ProtectedRoute rol="student" redirectTo="/login" />} />
+
       {/* Rutas de acceso unico a PROFESORES */}
       <Route element={<ProtectedRoute rol="teacher" redirectTo="/login" />}>
         <Route path="/professor-tour" element={<ProfessorTour />} />
-        <Route path="/dashboard/teacher" element={<h1>Dashboar Profesor</h1>} />
       </Route>
     </Routes>
   );

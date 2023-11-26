744d283cbdeef2b4c70ab4f59bed10da80096827
Mauwt <david.jacobo@utec.edu.pe>
2023-11-26 01:24:55 -0500

    axios conflict solve

--- src/api/configs/axiosConfig.ts
+++ src/api/configs/axiosConfig.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
   baseURL: 'https://proyecto-final-teachcall-back-production.up.railway.app/',
-=======
-  baseURL: "https://teachcall.azurewebsites.net/",
->>>>>>> 0105537bb29e672e5c63d4d7ed52c5846fb72a6f
 });
 
 export default api;
